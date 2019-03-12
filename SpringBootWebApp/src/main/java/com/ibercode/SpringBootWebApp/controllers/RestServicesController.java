9443986670d3fdb90aa6077a804e5acdba72b4da
ibercode <31028993+ibercode@users.noreply.github.com>
2019-03-12 17:30:18 +0100

    [R1314]: Added content to RestServiceController

--- SpringBootWebApp/src/main/java/com/ibercode/SpringBootWebApp/controllers/RestServicesController.java
+++ SpringBootWebApp/src/main/java/com/ibercode/SpringBootWebApp/controllers/RestServicesController.java
@@ -1,8 +1,13 @@
 package com.ibercode.SpringBootWebApp.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RestServicesController {
 
+    @GetMapping(path = "ws")
+    public String getRest() {
+	return "getrest";
+    }
 }
