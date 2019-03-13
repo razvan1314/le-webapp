44885b317ea108129e4f26d466dc3853b7b566b6
ibercode <31028993+ibercode@users.noreply.github.com>
2019-03-13 15:22:53 +0100

    [ibcd] UPDATE : Person Class getters and setters added

--- SpringBootWebApp/src/main/java/com/ibercode/SpringBootWebApp/model/Person.java
+++ SpringBootWebApp/src/main/java/com/ibercode/SpringBootWebApp/model/Person.java
@@ -17,4 +17,33 @@ public class Person {
 		this.lastName = lastName;
 	}
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + "]";
+	}
+
 }
