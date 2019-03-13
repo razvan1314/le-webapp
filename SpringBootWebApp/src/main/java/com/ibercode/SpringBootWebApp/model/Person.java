bcffdd9d79aa68d8165851127b39702e328469d1
ibercode <31028993+ibercode@users.noreply.github.com>
2019-03-13 15:22:06 +0100

    [ibcd]: UPDATE Person class - constructors added

--- SpringBootWebApp/src/main/java/com/ibercode/SpringBootWebApp/model/Person.java
+++ SpringBootWebApp/src/main/java/com/ibercode/SpringBootWebApp/model/Person.java
@@ -5,4 +5,16 @@ public class Person {
 	private int id;
 	private String firstName;
 	private String lastName;
+
+	public Person() {
+		super();
+	}
+
+	public Person(int id, String firstName, String lastName) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
 }
