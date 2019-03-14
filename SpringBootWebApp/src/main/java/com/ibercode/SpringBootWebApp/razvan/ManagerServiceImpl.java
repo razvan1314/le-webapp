97ba4abad197325070c6477f76270d8aedc5588d
ibercode <31028993+ibercode@users.noreply.github.com>
2019-03-14 10:25:47 +0100

    ManagerServiceImpl class added

---
+++ SpringBootWebApp/src/main/java/com/ibercode/SpringBootWebApp/razvan/ManagerServiceImpl.java
@@ -0,0 +1,14 @@
+package com.ibercode.SpringBootWebApp.razvan;
+
+import java.util.List;
+
+import com.ibercode.SpringBootWebApp.model.Manager;
+
+public class ManagerServiceImpl implements ManagerService{
+
+	@Override
+	public List<Manager> getAllManagers() {
+		return null;
+	}
+
+}
