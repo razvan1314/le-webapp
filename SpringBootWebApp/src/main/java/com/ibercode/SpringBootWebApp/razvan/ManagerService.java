17b67c752390c69ac8ec7c4ec0c88fa1956e01e9
ibercode <31028993+ibercode@users.noreply.github.com>
2019-03-14 10:24:47 +0100

    [ibcd]: New Pack Added

---
+++ SpringBootWebApp/src/main/java/com/ibercode/SpringBootWebApp/razvan/ManagerService.java
@@ -0,0 +1,10 @@
+package com.ibercode.SpringBootWebApp.razvan;
+
+import java.util.List;
+
+import com.ibercode.SpringBootWebApp.model.Manager;
+
+public interface ManagerService {
+
+	List<Manager> getAllManagers();
+}
